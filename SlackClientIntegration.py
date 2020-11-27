043b16c101d8a400a535f1f09790ce40cbdf8170
Nico Bautista <37518285+30574c@users.noreply.github.com>
2020-11-27 12:32:46 -0700

    Update to new Slack API

--- SlackClientIntegration.py
+++ SlackClientIntegration.py
@@ -1,85 +1,52 @@
 from slackclient import SlackClient
-import time
-import threading
-from threading import Thread
+import threading, time
 
-### DESCRIPTION ###
-# There already are slack API python wrappers but this code further simplifies the ability to just
-# post a message to a slack channel. All you have to do is install the above dependencies,
-# and use the below example code anywhere you want. The beauty of this code is that it
-# runs on its own thread and can handle a large queue of messages that might get backed up
-# due to the slack API rate limit restrictions.
-
-### EXAMPLE CODE TO USE ###
+### EXAMPLE ###
 # from SlackClientIntegration import Slack
-# slackObject = Slack('channel name')
-# slackObject.sendMessage("add string here")
-# slackObject.sendMessage("add as many more srings as you want")
-# slackObject.closeThread() 
+# from config import *
+# tradingUpdates = Slack('bot_testing',TRADINGUPDATEBOT_SLACK_TOKEN) # Token in config
+# tradingUpdates.sendMessage('MESSAGE')
+# tradingUpdates.close()
 
 class Slack:
 
-    def __init__(self, channelName):
-        self.slack_token_NAME1 = "xxxx-your_slack_token" #MODIFY
-        self.slack_token_NAME2 = "xxxx-your_slack_token" #MODIFY
+    def __init__(self, channelName, token):
+        self.slackToken = token
         self.channelName = channelName
         self.messageQueue = []
         self.threadExitSignal = threading.Event()
-        self.thread = Thread(target=self.sendMessageFromQueue)
+        self.thread = threading.Thread(target=self.sendMessageFromQueue)
         self.thread.start()
+        self.count = 0
 
     def sendMessageFromQueue(self):
-
         try:
-            ### VARIABLES ###
-            slack_token = ''
-            slack_channel_id = ''
+            ### PREPARE CLIENT ###
+            client = SlackClient(self.slackToken)
 
-            # SET UP TOKEN AND NAME (ONLY MODIFY THIS SECTION FOR NEW CHANNELS)
-            if ('your_channel_name' in self.channelName): #MODIFY
-                slack_token = self.slack_token_NAME1 #MODIFY
-
-            elif ('your_other_channel_name' in self.channelName): #MODIFY
-                slack_token = self.slack_token_NAME2 #MODIFY
-
-            slack_client = SlackClient(slack_token)
-
-            # RETRIEVE CHANNEL ID
-            channels = slack_client.api_call("channels.list", exclude_archived=1)['channels']
+            ### RETRIEVE CHANNEL ID ###
+            channels = client.api_call('conversations.list')['channels']
             for channel in channels:
-                if channel.get('name') == self.channelName:
-                    slack_channel_id = channel.get('id')
+                if channel['name'] == self.channelName:
+                    channelID = channel.get('id')
                     break
 
-        except Exception as error:
-            print("ERROR: Class Slack , sendMessageFromQueue ... {0}".format(error))
-
-        while self.messageQueue or not self.threadExitSignal.is_set():
-
-            try:
-                if (self.messageQueue):
-
+            while not self.threadExitSignal.is_set() or not self.count:
+                while self.messageQueue:
                     ### POP MESSAGE FROM QUEUE ###
                     queueMessage = self.messageQueue.pop(0)
 
-                    ### VARIABLES ###
-                    slack_message = queueMessage
-
                     ### SEND MESSAGE ###
+                    client.api_call('chat.postMessage',channel=channelID,text=queueMessage)
                     time.sleep(1)
-                    slack_client.api_call(
-                        "chat.postMessage",
-                        channel = slack_channel_id,
-                        text = slack_message
-                    )
+                    self.count += 1
 
-            except Exception as error:
-                print ("ERROR: Class Slack , sendMessageFromQueue ... {0}".format(error))
+        except Exception as error:
+            print("ERROR: Class Slack , sendMessageFromQueue ... {error}".format(error=error))
 
     def sendMessage(self, message):
         self.messageQueue.append(message)
 
-    def closeThread(self):
+    def close(self):
         self.threadExitSignal.set()
-        print("Waiting for Slack thread to finish submitting messages, if any.")
         self.thread.join()
